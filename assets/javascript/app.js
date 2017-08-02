d31bae146fbf720e93473939d00a7732f245695d
jcprice12 <jcprice12@gmail.com>
2017-08-01 22:59:04 -0400

    added ability to create 'new screen' to show correct answer and to tell you if you got the question right. Added a lot of styling (forgot to stage changes)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,6 @@ function Question(timeLimit, question, choices, correctAnswer){
 	this.getCorrectAnswer = function(){
 		return correctAnswer;
 	}
-
 }
 
 var game = {
@@ -25,9 +24,11 @@ var game = {
 	currentQuestionIndex: null,
 	questionTimer: null,
 	score: null,
-	timeBetweenRounds: 7000,
+	timeBetweenRounds: 3000,
+	inTransition: false,
 	startGame: function(){
 		this.questions = instantiateQuestions();
+		this.inTransition = false;
 		this.currentQuestion = this.questions[0];
 		this.currentQuestionIndex = 0;
 		this.questionTimer = null;
@@ -37,90 +38,134 @@ var game = {
 	},
 	startTimer: function(secondsRemaining){
 		console.log(secondsRemaining);
+		var timeContainer = $("#timeContainer");
+		timeContainer.html(this.timeConverter(secondsRemaining));
 		if(secondsRemaining > 0){
 			this.questionTimer = setTimeout(function(){
 				secondsRemaining --;
 				game.startTimer(secondsRemaining);
 			},1000);
 		} else {
-			this.executeOutOfTime();
+			this.buildNewScreen("You ran out of time!", "");
 		}
 	},
 	compareAnswer: function(choiceIndex){
-		if(choiceIndex === this.currentQuestion.getCorrectAnswer()){
-			this.executeCorrectResponse();
-		} else {
-			this.executeIncorrectResponse();
-		}
-	},
-	executeCorrectResponse: function(){
-		console.log("you got it right.");
-		this.score++;
-		this.buildCorrectScreen();
-		this.executeNextRound();
-	},
-	executeIncorrectResponse: function(){
-		console.log("you answered incorrectly.");
-		this.buildIncorrectScreen();
-		this.executeNextRound();
-	},
-	executeOutOfTime: function(){
-		console.log("you ran out of time.");
-		this.buildOutOfTimeScreen();
-		this.executeNextRound();
-	},
-	executeNextRound: function(){
-		console.log("starting next round.");
 		clearTimeout(this.questionTimer);
-		this.currentQuestionIndex++;
-		setTimeout(function(){
-			if(game.currentQuestionIndex < (game.questions.length)){
-				game.currentQuestion = game.questions[game.currentQuestionIndex];
-				game.buildNextQuestion(game.currentQuestion);
-				game.startTimer(game.currentQuestion.getTimeLimit());
-			} else {
-				game.executeCompletionState();
-			}
-		},this.timeBetweenRounds);
+		if(choiceIndex === this.currentQuestion.getCorrectAnswer()){
+			this.score++;
+			this.buildNewScreen("You are correct!", "");
+		} else {
+			this.buildNewScreen("You are incorrect!", "");
+		}
 	},
 	executeCompletionState: function(){
 		console.log("You completed the game.");
-		console.log("Your score was " + this.score + " out of " + this.questions.length);
+		console.log("Your score was " + game.score + " out of " + game.questions.length);
+		$("#timeContainer").html("<p>You completed the game!</p>");
+		$("#questionContainer").html("<p>Your score was " + game.score + " out of " + game.questions.length + "</p>");
+		$("#questionContainer").css("text-align", "center");
+		$("#questionContainer").css("margin-left", "0px");
+		var playAgain = document.createElement("div");
+		$(playAgain).html("Play Again");
+		$(playAgain).addClass("playAgain");
+		playAgain.addEventListener("click", function (){
+			game.startGame();
+		});
+		$("#answerChoicesContainer").html("");
+		$("#answerChoicesContainer").append($(playAgain));
 	},
 	buildNextQuestion: function(question){
 		console.log("building new question...");
 		var timeContainer = $("#timeContainer");
 		timeContainer.html(question.getTimeLimit());
+		timeContainer.css("text-align", "center");
 		var questionContainer = $("#questionContainer");
 		questionContainer.html(question.getQuestion());
+		questionContainer.css("text-align", "left");
+		questionContainer.css("margin-left", "0px");
 		var answerChoicesContainer = $("#answerChoicesContainer");
 		answerChoicesContainer.html("");
 		for(i = 0; i < question.getChoices().length; i++){
-			console.log(question.getChoices()[i]);
 			var choiceDiv = $("<div>");
+			var answerChoiceBox = $("<div>");
+			answerChoiceBox.addClass("answerChoiceBox");
 			choiceDiv.addClass("answerChoiceContainer");
 			choiceDiv.attr("data-choice-index",i.toString());
 			choiceDiv.html(question.getChoices()[i]);
-			answerChoicesContainer.append(choiceDiv);
+			answerChoiceBox.append(choiceDiv);
+			answerChoicesContainer.append(answerChoiceBox);
 		}
 
 	},
-	buildCorrectScreen: function(){
-		console.log("building correct screen.");
-	},
-	buildIncorrectScreen: function(){
-		console.log("building incorrect screen.");
-	},
-	buildOutOfTimeScreen: function(){
-		console.log("building out of time screen");
-	},
+	timeConverter: function(t) {//this looks familiar :D
+	    //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+	    var minutes = Math.floor(t / 60);
+	    var seconds = t - (minutes * 60);
+
+	    if (seconds < 10) {
+	      seconds = "0" + seconds;
+	    }
+
+	    if (minutes === 0) {
+	      minutes = "00";
+	    }
+
+	    else if (minutes < 10) {
+	      minutes = "0" + minutes;
+	    }
+
+	    return minutes + ":" + seconds;
+  	},
+  	buildNewScreen: function(title, imageSource){
+  		console.log("building new screen");
+  		this.inTransition = true;
+  		//$("#overlay").css("height","100%");
+  		$("#overlayTitle").html(title);
+  		$("#overlayAnswer").html("Correct Answer: " + this.currentQuestion.getChoices()[this.currentQuestion.getCorrectAnswer()]);
+  		$("#overlay").animate({
+  			"top": 0,
+  		},1000, function(){
+  			game.currentQuestionIndex++;
+  			if(game.currentQuestionIndex < (game.questions.length)){
+	  			game.currentQuestion = game.questions[game.currentQuestionIndex];
+				game.buildNextQuestion(game.currentQuestion);
+				if($(window).height() <= $("body").height()){
+					var myHeight = $("body").height();
+				} else {
+					var myHeight = $(window).height();
+				}
+				$("#overlay").css("height",myHeight);
+				$("#timeContainer").html(game.timeConverter(game.currentQuestion.getTimeLimit()));
+				setTimeout(function(){
+					game.removeOverlay(game.goToNextQuestion);
+				},game.timeBetweenRounds);
+			} else {
+				game.executeCompletionState();
+				setTimeout(function(){
+					game.removeOverlay("empty");
+				},game.timeBetweenRounds);
+			}
+  		});
+  	},
+  	removeOverlay: function(callback){
+  		var myHeight = $("#overlay").height() * -1;
+  		$("#overlay").animate({
+  			"top": myHeight,
+  		},1000, function(){
+  			if(typeof callback === "function"){
+  				callback();
+  			}
+  		});
+  	},
+  	goToNextQuestion: function(){
+  		game.inTransition = false;
+		game.startTimer(game.currentQuestion.getTimeLimit());
+  	},
 }
 
 function instantiateQuestions(){
 	var questions = [];
-	questions.push(new Question(10, "Which one of these characters is a \"Bad Guy?\"", ["Boba Fett", "Chewbacca", "Luke Skywalker"], 0));
-	questions.push(new Question(10, "Which one of these characters is a \"Good Guy?\"", ["Darth Vader", "Darth Maul", "Luke Skywalker"], 2));
-	console.log(questions);
+	questions.push(new Question(30, "Which one of these characters is a \"Bad Guy?\"", ["Boba Fett", "Chewbacca", "Luke Skywalker"], 0));
 	return questions;
 }
 
@@ -128,7 +173,9 @@ $(document).ready(function(){
 	game.startGame();
 
 	$(document).on('click', ('.answerChoiceContainer'), function() {
-    	var choiceIndex = ($(this).attr("data-choice-index") * 1);//this is pretty hilarious
-    	game.compareAnswer(choiceIndex);
+		if(game.inTransition === false) {
+	    	var choiceIndex = ($(this).attr("data-choice-index") * 1);//this is pretty hilarious
+	    	game.compareAnswer(choiceIndex);
+    	}
     });
 });
